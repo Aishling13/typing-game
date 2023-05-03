b1dd49536fdbc641529e52b0aa136c4b304ef20a
Chloe Lodge <chloe.lodge@outlook.com>
2023-05-03 21:45:49 +0100

    added 404 page

---
+++ src/pages/NoPage/NoPage.jsx
@@ -0,0 +1,13 @@
+import '../NoPage/NoPage.css'; 
+
+const NoPage = () => {
+  return (
+    <div className="NoPage">
+    <h1 className="NoPage-header"
+    >
+    404</h1>
+  </div>
+  )
+};
+
+export default NoPage;
